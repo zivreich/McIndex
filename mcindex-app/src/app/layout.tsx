d4d109feda694bd796565bccb2a90f13681394fa
zivreich <cooliman1300@gmail.com>
2025-05-17 19:28:16 +0300

    Deployment error fixes

--- mcindex-app/src/app/layout.tsx
+++ mcindex-app/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import { StockTicker } from "./components/global/Ticker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
